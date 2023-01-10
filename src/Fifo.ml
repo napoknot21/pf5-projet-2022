aefad8138969e30c8ee9b7b7b8736512235368f5
napoknot21 <kinbrian5@hotmail.com>
2023-01-10 18:37:38 +0100

    implementation changed

--- src/Fifo.ml
+++ src/Fifo.ml
@@ -8,20 +8,28 @@
 
 *)
 
-type 'a t = 'a list (* head of list = first out *)
+type 'a t = 'a list * 'a list (* head of list = first out *)
 
-let empty = [];;
+let empty = ([], []);;
 
-let push x q = x::q;;
+let isEmpty (l: 'a t ) = match l with
+  | ([], []) -> true
+  | (_,_) -> false;;
 
-let pop q = match q with 
-  | x::q' -> x, q' 
-  | [] -> raise Not_found
+let push (x: 'a) (l : 'a t ) = match l with
+  | (l1, l2) -> (l1, x::l2);;
 
-let of_list l =
-  let rec aux list_origin list_rev = match list_origin with
-    | [] -> list_rev
-    | e::rest -> aux rest (e::list_rev)
-  in aux l [];;  
+let rec pushList (x: 'a list) (l : 'a t) = match x with
+  | [] -> l
+  | h::tail -> pushList (tail) (push h l);;  
 
-let to_list l = l
+let pop (q : 'a t ) = match q with 
+  | ([],[]) -> raise Not_found
+  | (h::tail,l2)-> h, (tail,l2)
+  | ([], l2) -> match List.rev l2 with
+    | [] -> assert false 
+    | h::tail -> h, (tail,[]);;
+
+let of_list (l: 'a list) = pushList l empty;;
+
+let to_list (l: 'a t) = match l with | (l1,l2) -> l1 @ List.rev l2;;
